10783a3d32df0292defe1d6eb6ca208feb8f2bcf
leejongsuu <lik9819@naver.com>
2024-02-16 22:01:16 +0900

    GOF Pattern

---
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/code/CacheProxy.java
@@ -0,0 +1,24 @@
+package hello.proxy.pureproxy.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CacheProxy implements Subject {
+
+    private Subject target;
+    private String cacheValue;
+
+    // proxy가 realSubject를 참조하려면 생성자를 통해 의존관계를 주입받아줘야함.
+    public CacheProxy(Subject target) {
+        this.target = target;
+    }
+
+    @Override
+    public String operation() {
+        log.info("프록시 호출");
+        if (cacheValue == null) {
+            cacheValue = target.operation();
+        }
+        return cacheValue;
+    }
+}
