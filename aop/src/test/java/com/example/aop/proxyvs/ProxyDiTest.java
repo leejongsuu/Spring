8eaa7da8aadd1d56ab00e2b65df4454fe20c9f8f
leejongsuu <lik9819@naver.com>
2024-02-26 18:10:14 +0900

    feat: proxy Di

---
+++ aop/src/test/java/com/example/aop/proxyvs/ProxyDiTest.java
@@ -0,0 +1,30 @@
+package com.example.aop.proxyvs;
+
+import com.example.aop.member.MemberService;
+import com.example.aop.member.MemberServiceImpl;
+import com.example.aop.proxyvs.code.ProxyDiAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) //JDK 동적 프록시, DI 예외 발생
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) //CGLIB 프록시, 성공
+@Import(ProxyDiAspect.class)
+public class ProxyDiTest {
+
+    @Autowired
+    MemberService memberService; //JDK 동적 프록시 OK, CGLIB OK
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl; //JDK 동적 프록시 X, CGLIB OK
+
+    @Test
+    void go() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
+    }
+}
