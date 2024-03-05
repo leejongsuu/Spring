26a5e7976dcaf1464fed594a6508496823b58d01
leejongsuu <lik9819@naver.com>
2024-03-05 20:25:03 +0900

    feat: ConfigurationProperties 추가

--- external/src/main/java/hello/external/ExternalApplication.java
+++ external/src/main/java/hello/external/ExternalApplication.java
@@ -1,12 +1,16 @@
 package hello.external;
 
 import hello.external.config.MyDataSourceEnvConfig;
+import hello.external.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
-@Import({MyDataSourceEnvConfig.class})
+//@Import({MyDataSourceEnvConfig.class})
+//@Import({MyDataSourceValueConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.external.datasource")
+@ConfigurationPropertiesScan
 public class ExternalApplication {
 
 	public static void main(String[] args) {
